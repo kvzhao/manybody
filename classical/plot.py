fe161c580729f11a35afedf0c65aa580b31ffa55
kvzhao <kelispinor@gmail.com>
2017-10-01 23:33:11 +0800

    Plot the absolute mag

--- classical/plot.py
+++ classical/plot.py
@@ -19,7 +19,7 @@ E = data["E"][:]
 E2 = data["E2"][:] 
 E4 = data["E4"][:] 
 M = data["M"][:]
-#absM = data["absM"][:]
+absM = data["absM"][:]
 M2 = data["M2"][:]
 M4 = data["M4"][:]
 
@@ -28,7 +28,6 @@ X  = (M2 - M**2) / T
 R2 = M4 / M2 ** 2
 # Binder Cumulant
 U2 = 1.5 - 0.5 * R2
-
 
 def plot_and_save_figure(path, name, T, observable):
     plt.plot(T, observable)
@@ -44,3 +43,4 @@ plot_and_save_figure(FLAGS.out_dir, "Heat Capacity", T, Cv)
 plot_and_save_figure(FLAGS.out_dir, "Susceptibility", T, X)
 plot_and_save_figure(FLAGS.out_dir, "Energy", T, E)
 plot_and_save_figure(FLAGS.out_dir, "Magnetization", T, M)
+plot_and_save_figure(FLAGS.out_dir, "|Magnetization|", T, absM)
