d4bb3f84da8f0350375ba096d480a7bbf7f02fb7
kvzhao <kelispinor@gmail.com>
2017-10-02 16:54:42 +0800

    Plot observables with different system sizes

---
+++ classical/plot_observable_vs_size.py
@@ -0,0 +1,48 @@
+import os, sys
+
+from os.path import isfile, join
+from os import listdir
+
+import argparse
+import h5py as hf
+import numpy as np
+import matplotlib.pyplot as plt
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--observ", dest="observ", default="M2", type=str, help="Physical Observable")
+parser.add_argument("--data_dir", dest="data_dir", default="from_boromir", type=str, help="Folder of saved resutls")
+parser.add_argument("--out_name", dest="out_name", default="out", type=str, help="Save figure name")
+FLAGS = parser.parse_args()
+
+# Read all data list from some folder
+datafiles = [f for f in listdir(FLAGS.data_dir) if isfile(join(FLAGS.data_dir, f))]
+datanames = [fn.rstrip(".h5") for fn in datafiles]
+
+for df in datafiles:
+    data = hf.File(join(FLAGS.data_dir, df), "r")
+    T = data["T"][:]
+    if FLAGS.observ == 'Cv':
+        E = data["E"][:] 
+        E2 = data["E2"][:] 
+        observ = (E2 - E**2) / T ** 2
+    elif FLAGS.observ == "X":
+        #M = data["M"][:]
+        M = data["absM"][:]
+        M2 = data["M2"][:]
+        observ  = (M2 - M**2) / T 
+    elif FLAGS.observ == "U2":
+        M2 = data["M2"][:]
+        M4 = data["M4"][:]
+        R2 = M4 / M2 ** 2
+        # Binder Cumulant
+        observ = 1.5 - 0.5 * R2
+    else:
+        observ = data[FLAGS.observ][:]
+    plt.plot(T, observ, '-*', linewidth=1.0)
+
+plt.xlabel("T")
+plt.ylabel(FLAGS.observ)
+plt.legend(datanames)
+plt.grid(True)
+plt.title(FLAGS.out_name)
+plt.savefig(FLAGS.out_name + ".png")
---
+++ classical/plotall.sh
@@ -0,0 +1,6 @@
+RESFILE="results"
+python3 plot_observable_vs_size.py --observ=E --data_dir=${RESFILE} --out_name=figures/Energy
+python3 plot_observable_vs_size.py --observ=absM --data_dir=${RESFILE} --out_name=figures/AbsoluteMagnetization
+python3 plot_observable_vs_size.py --observ=M2 --data_dir=${RESFILE} --out_name=figures/Magnetization
+python3 plot_observable_vs_size.py --observ=Cv --data_dir=${RESFILE} --out_name=figures/HeatCapacity
+python3 plot_observable_vs_size.py --observ=U2 --data_dir=${RESFILE} --out_name=figures/BinderRatio
